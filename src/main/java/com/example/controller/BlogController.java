c221c71571b218e85bb890de1b94633d66bac6d4
chunghanH <yous421@gmail.com>
2019-08-17 21:09:01 +0800

    remove allowed origins

--- src/main/java/com/example/controller/BlogController.java
+++ src/main/java/com/example/controller/BlogController.java
@@ -6,7 +6,6 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Article;
 import com.example.service.BlogService;
@@ -26,38 +24,32 @@ public class BlogController {
     @Resource
     private BlogService blogService; 
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("text", "index");
         return "index";
     }    
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @GetMapping("/api/articles")
     public @ResponseBody List<Article> articles(){
         return blogService.articles();
     }
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @GetMapping("/api/articles/{id}")
     public @ResponseBody Article article(@PathVariable("id") long id){
         return blogService.article(id);
     }
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @PostMapping("/api/articles")
     public @ResponseBody void create(@RequestBody Article article){
         blogService.create(article);
     }
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @PutMapping("/api/articles/{id}")
     public @ResponseBody void update(@RequestBody Article article){
         blogService.update(article);
     }
     
-    @CrossOrigin(origins = "http://chunghanh.github.io")
     @DeleteMapping("/api/articles/{id}")
     public @ResponseBody void delete(@PathVariable("id") long id){
         blogService.delete(id);
